9205aebe0fe663edec67d437dcc72ae20561f9f3
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-04-10 10:58:59 -0500

    LineEndings as LF rather than CRLF

--- src/main/java/net/visualillusionsent/utils/DateUtils.java
+++ src/main/java/net/visualillusionsent/utils/DateUtils.java
@@ -31,23 +31,20 @@ import java.util.TimeZone;
  * @version 1.0
  * @author Jason (darkdiplomat)
  */
-public final class DateUtils {
+public final class DateUtils{
 
     /**
      * Date Format as dd-MMM-yyyy
      */
-    private static DateFormat date_form = new SimpleDateFormat("dd-MMM-yyyy");
-
+    private static DateFormat date_form     = new SimpleDateFormat("dd-MMM-yyyy");
     /**
      * Date Format as HH:mm:ss
      */
-    private static DateFormat time_form = new SimpleDateFormat("HH:mm:ss");
-
+    private static DateFormat time_form     = new SimpleDateFormat("HH:mm:ss");
     /**
      * Date Format as dd-MMM-yyyy HH:mm:ss
      */
     private static DateFormat datetime_form = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
-
     /**
      * Date Format as HH:mm:ss dd-MMM-yyyy
      */
@@ -56,7 +53,7 @@ public final class DateUtils {
     /**
      * This class should never be constructed
      */
-    private DateUtils() {}
+    private DateUtils(){}
 
     /**
      * Returns the Date as a {@link String} formatted as dd-MMM-yyyy
@@ -68,7 +65,7 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToDate(long time) throws UtilityException {
+    public static final String longToDate(long time) throws UtilityException{
         return longToDate(time, TimeZone.getDefault());
     }
 
@@ -84,13 +81,12 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToDate(long time, TimeZone zone) throws UtilityException {
+    public static final String longToDate(long time, TimeZone zone) throws UtilityException{
         //Is time negative?
-        if (time < 0) {
+        if(time < 0){
             throw new UtilityException("Time cannot be a negative number");
         }
-
-        if (zone != null && zone != TimeZone.getDefault()) {
+        if(zone != null && zone != TimeZone.getDefault()){
             date_form.setTimeZone(zone);
         }
         return date_form.format(parseCal(time, zone).getTime());
@@ -106,7 +102,7 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToTime(long time) throws UtilityException {
+    public static final String longToTime(long time) throws UtilityException{
         return longToTime(time, TimeZone.getDefault());
     }
 
@@ -120,13 +116,12 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToTime(long time, TimeZone zone) throws UtilityException {
+    public static final String longToTime(long time, TimeZone zone) throws UtilityException{
         //Is time negative?
-        if (time < 0) {
+        if(time < 0){
             throw new UtilityException("Time cannot be a negative number");
         }
-
-        if (zone != null && zone != TimeZone.getDefault()) {
+        if(zone != null && zone != TimeZone.getDefault()){
             time_form.setTimeZone(zone);
         }
         return time_form.format(parseCal(time, zone).getTime());
@@ -142,7 +137,7 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToDateTime(long time) throws UtilityException {
+    public static final String longToDateTime(long time) throws UtilityException{
         return longToDateTime(time, TimeZone.getDefault());
     }
 
@@ -158,13 +153,12 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToDateTime(long time, TimeZone zone) throws UtilityException {
+    public static final String longToDateTime(long time, TimeZone zone) throws UtilityException{
         // Is time negative?
-        if (time < 0) {
+        if(time < 0){
             throw new UtilityException("Time cannot be a negative number");
         }
-
-        if (zone != null && zone != TimeZone.getDefault()) {
+        if(zone != null && zone != TimeZone.getDefault()){
             datetime_form.setTimeZone(zone);
         }
         return datetime_form.format(parseCal(time, zone).getTime());
@@ -180,7 +174,7 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToTimeDate(long time) throws UtilityException {
+    public static final String longToTimeDate(long time) throws UtilityException{
         return longToTimeDate(time, TimeZone.getDefault());
     }
 
@@ -196,13 +190,12 @@ public final class DateUtils {
      * <br>
      *             if time is less than 0
      */
-    public static final String longToTimeDate(long time, TimeZone zone) throws UtilityException {
+    public static final String longToTimeDate(long time, TimeZone zone) throws UtilityException{
         // Is time negative?
-        if (time < 0) {
+        if(time < 0){
             throw new UtilityException("Time cannot be a negative number");
         }
-
-        if (zone != null && zone != TimeZone.getDefault()) {
+        if(zone != null && zone != TimeZone.getDefault()){
             timedate_form.setTimeZone(zone);
         }
         return timedate_form.format(parseCal(time, zone).getTime());
@@ -221,29 +214,26 @@ public final class DateUtils {
      *             if time is less than 0<br>
      *             if the format is invalid or {@code null}
      */
-    public static final String longToFormatedDateTime(long time, String format) throws UtilityException {
+    public static final String longToFormatedDateTime(long time, String format) throws UtilityException{
         //Is time negative?
-        if (time < 0) {
+        if(time < 0){
             throw new UtilityException("Time cannot be a negative number");
         }
         //Is format null?
-        else if (format == null) {
+        else if(format == null){
             throw new UtilityException("arg.null", "format");
         }
-
         //Create a Date object from the time given
         Date date = new Date(time);
-
         //Create DateFormat object and verify the format is valid
         DateFormat form;
-        try {
+        try{
             form = new SimpleDateFormat(format);
         }
-        catch (IllegalArgumentException iae) {
+        catch(IllegalArgumentException iae){
             //Invalid format throws an UtilityException
             throw new UtilityException("Invalid format syntax");
         }
-
         //Format it and return
         return form.format(date);
     }
@@ -256,30 +246,28 @@ public final class DateUtils {
      * @return {@link Date} parsed from the date parameter
      * @throws UtilityException
      */
-    public static final Date getDateFromString(String date) throws UtilityException {
+    public static final Date getDateFromString(String date) throws UtilityException{
         Date theDate = null;
-        try {
+        try{
             theDate = datetime_form.parse(date);
         }
-        catch (ParseException pe) {
+        catch(ParseException pe){
             throw new UtilityException(pe.getMessage());
         }
         return theDate;
     }
 
-    private final static Calendar parseCal(long time, TimeZone to) {
+    private final static Calendar parseCal(long time, TimeZone to){
         Calendar calendar = Calendar.getInstance();
         TimeZone fromTimeZone = calendar.getTimeZone();
         TimeZone toTimeZone = to != null ? to : TimeZone.getTimeZone("GMT");
-
         calendar.setTimeZone(fromTimeZone);
         calendar.add(Calendar.MILLISECOND, fromTimeZone.getRawOffset() * -1);
-        if (fromTimeZone.inDaylightTime(calendar.getTime())) {
+        if(fromTimeZone.inDaylightTime(calendar.getTime())){
             calendar.add(Calendar.MILLISECOND, calendar.getTimeZone().getDSTSavings() * -1);
         }
-
         calendar.add(Calendar.MILLISECOND, toTimeZone.getRawOffset());
-        if (toTimeZone.inDaylightTime(calendar.getTime())) {
+        if(toTimeZone.inDaylightTime(calendar.getTime())){
             calendar.add(Calendar.MILLISECOND, toTimeZone.getDSTSavings());
         }
         calendar.setTimeInMillis(time);
